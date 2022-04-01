4ac756ce183282315b99aeb0f5577af29e24758a
Jason Au <59789630+jasonau2003@users.noreply.github.com>
2022-04-02 02:30:28 +0800

    Update index.vue

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -90,7 +90,7 @@
 import { ref } from '@vue/reactivity';
 import { h, onMounted, watch } from '@vue/runtime-core';
 import { NLayout, NLayoutSider, NLayoutContent, NMenu, MenuOption, NIcon, NProgress, NText, NModal, NCard, NInput, NButton, NScrollbar, NTime, NTooltip, useDialog } from 'naive-ui'
-import { File, Trash, CircleX, Logout, Settings, Copy, Video, Camera } from '@vicons/tabler'
+import { File, Trash, CircleX, Logout, Settings, Copy, Video, Camera, Share } from '@vicons/tabler'
 import http from '../../utils/axios'
 import { byteConvert } from '../../utils'
 import { useRoute, useRouter } from 'vue-router'
@@ -129,7 +129,7 @@ import { useRoute, useRouter } from 'vue-router'
     {
       label: '资源库',
       key: 'share',
-      icon: renderIcon(Copy)
+      icon: renderIcon(Share)
     },
     {
       label: '设置',
